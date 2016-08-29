559e510116acb86ec5c4234b7c684d47f001bc37
Anderson Santos <anderson.bcc.uag@gmail.com>
2016-08-29 17:47:14 -0300

    troca de imagens e mostrar imagem original com os descritores

--- modulo 1/run_orb.py
+++ modulo 1/run_orb.py
@@ -1,9 +1,15 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('horse.jpg', 0)
+img = cv2.imread('cow.jpg', 0)
 orb = cv2.ORB_create()
 kp, descriptors = orb.detectAndCompute(img, None)
 img_keypoints = cv2.drawKeypoints(img, kp, None, flags=2)
-plt.imshow(img_keypoints)
+
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Imagem original'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(122), plt.imshow(img_keypoints, cmap='gray')
+plt.title('Imagem com descritores'), plt.xticks([]), plt.yticks([])
+
 plt.show()
