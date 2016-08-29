d4ae15bd7847b0bfb59555ba72c30494dd193296
Anderson Santos <anderson.bcc.uag@gmail.com>
2016-08-29 16:55:11 -0300

    renomeação de variáveis

--- modulo 3/extract_features.py
+++ modulo 3/extract_features.py
@@ -14,79 +14,77 @@ from skimage import data
 from skimage.morphology import disk
 from skimage.util import img_as_ubyte
 from skimage.color import rgb2gray
-import scipy.stats as SCI
+import scipy.stats as sci
 
 
 path = "path_concepts_here"
 fig = plt.figure(figsize=(5, 2))
 for conceito in [2]:  # [1:10]
-    mypath = path + str(conceito) + '/Background Facil/'
+    my_path = path + str(conceito) + '/Background Facil/'
     # for n in range(10):
     #    print path + str(conceito) + '\Background Facil\\' + str(n)
-    #    onlyfiles = [f for f in listdir(unicode(mypath, 'utf-8')) if isfile(join(unicode(mypath, 'utf-8'), f))]
+    #    onlyfiles = [f for f in listdir(unicode(my_path, 'utf-8')) if isfile(join(unicode(my_path, 'utf-8'), f))]
     #    print onlyfiles
     print '\t \n Conceito: ' + str(conceito) + '\n'
-
-    print unicode(mypath, 'utf-8')
-    print listdir(unicode(mypath, 'utf-8'))
+    print unicode(my_path, 'utf-8')
+    print listdir(unicode(my_path, 'utf-8'))
     i = 1
-    FEATURES = np.zeros([len(listdir(unicode(mypath, 'utf-8')))-1, 5])
+    features = np.zeros([len(listdir(unicode(my_path, 'utf-8')))-1, 5])
     radius = 220
     selem = disk(radius)
-    for f in listdir(unicode(mypath, 'utf-8')):
-        if isfile(join(unicode(mypath, 'utf-8'), f)):
+    for f in listdir(unicode(my_path, 'utf-8')):
+        if isfile(join(unicode(my_path, 'utf-8'), f)):
             if f != "desktop.ini":
-                # print unicode(mypath, 'utf-8')+f
-                img = mpimg.imread(unicode(mypath, 'utf-8')+f)
-                # imgGray = rgb2gray(img)
-                imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                # plt.imshow(imgGray, cmap='gray')
+                # print unicode(my_path, 'utf-8')+f
+                img = mpimg.imread(unicode(my_path, 'utf-8')+f)
+                # img_gray = rgb2gray(img)
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                # plt.imshow(img_gray, cmap='gray')
                 # plt.show()
-                threshold = filters.threshold_otsu(imgGray)
-                # threshold = filters.threshold_isodata(imgGray)
-                # threshold = filters.threshold_yen(imgGray)
+                threshold = filters.threshold_otsu(img_gray)
+                # threshold = filters.threshold_isodata(img_gray)
+                # threshold = filters.threshold_yen(img_gray)
                 if conceito in [7]:
                     if i in [4, 5, 7, 10]:
-                        imLim = imgGray < threshold
+                        im_lim = img_gray < threshold
                     else:
-                        imLim = imgGray >= threshold
+                        im_lim = img_gray >= threshold
                 elif conceito in [8]:
                     if i in [2, 4, 6, 10]:
-                        imLim = imgGray < threshold
+                        im_lim = img_gray < threshold
                     else:
-                        imLim = imgGray >= threshold
+                        im_lim = img_gray >= threshold
                 else:
-                    imLim = imgGray >= threshold
+                    im_lim = img_gray >= threshold
 
-                # imLim = rank.otsu(cv2.convertScaleAbs(imgGray), selem)
+                # im_lim = rank.otsu(cv2.convertScaleAbs(img_gray), selem)
                 ax1 = fig.add_subplot(5, 2, i)
-                plt.subplot(ax1), plt.imshow(imLim, cmap='binary')
+                plt.subplot(ax1), plt.imshow(im_lim, cmap='binary')
                 plt.title(f), plt.xticks([]), plt.yticks([])
 
                 color = np.array(img)
-                color[:, :, 0] = np.multiply(color[:, :, 0], imLim)
-                color[:, :, 1] = np.multiply(color[:, :, 1], imLim)
-                color[:, :, 2] = np.multiply(color[:, :, 2], imLim)
+                color[:, :, 0] = np.multiply(color[:, :, 0], im_lim)
+                color[:, :, 1] = np.multiply(color[:, :, 1], im_lim)
+                color[:, :, 2] = np.multiply(color[:, :, 2], im_lim)
 
-                regionR = color[color[:, :, 0] > 0, 0]
-                regionG = color[color[:, :, 1] > 0, 1]
-                regionB = color[color[:, :, 2] > 0, 2]
+                region_r = color[color[:, :, 0] > 0, 0]
+                region_g = color[color[:, :, 1] > 0, 1]
+                region_b = color[color[:, :, 2] > 0, 2]
 
-                MEAN = np.mean(regionR) + np.mean(regionG) + np.mean(regionB)
-                VARIANCE = np.var(regionR) + np.var(regionG) + np.var(regionB)
-                SKEWNESS = SCI.skew(regionR) + SCI.skew(regionG) + SCI.skew(regionB)
-                KURTOSIS = SCI.kurtosis(regionR) + SCI.kurtosis(regionG) + SCI.kurtosis(regionB)
-                ENTROPY = SCI.entropy(regionR) + SCI.entropy(regionG) + SCI.entropy(regionB)
+                mean = np.mean(region_r) + np.mean(region_g) + np.mean(region_b)
+                variance = np.var(region_r) + np.var(region_g) + np.var(region_b)
+                skewness = sci.skew(region_r) + sci.skew(region_g) + sci.skew(region_b)
+                kurtosis = sci.kurtosis(region_r) + sci.kurtosis(region_g) + sci.kurtosis(region_b)
+                entropy = sci.entropy(region_r) + sci.entropy(region_g) + sci.entropy(region_b)
 
-                FEATURES[i-1, 0] = MEAN
-                FEATURES[i-1, 1] = VARIANCE
-                FEATURES[i-1, 2] = SKEWNESS
-                FEATURES[i-1, 3] = KURTOSIS
-                FEATURES[i-1, 4] = ENTROPY
+                features[i-1, 0] = mean
+                features[i-1, 1] = variance
+                features[i-1, 2] = skewness
+                features[i-1, 3] = kurtosis
+                features[i-1, 4] = entropy
 
-                print FEATURES[i-1, :]
+                print features[i-1, :]
                 i = i + 1
 
     plt.show()
-    np.save(('space_feature\features_' + str(conceito)), FEATURES)
-    print 'FIM'
+    np.save(('space_feature\features_' + str(conceito)), features)
--- modulo 3/segmentation.py
+++ modulo 3/segmentation.py
@@ -18,14 +18,12 @@ plt.imshow(img)
 plt.show()
 
 # SLIC
-# segments_slic = slic(img, n_segments=200.0, compactness=10.0)
 segments_slic = slic(img, n_segments=1000, compactness=10.0, sigma=3.0)
-
 plt.imshow(mark_boundaries(img, segments_slic))
 plt.show()
 
-FEATURES = np.zeros([len(np.unique(segments_slic)), 4])
-IMAGE_SEG = np.array(img)
+features = np.zeros([len(np.unique(segments_slic)), 4])
+image_seg = np.array(img)
 for j in range(len(np.unique(segments_slic))):
 
     color = np.array(img)
@@ -35,63 +33,63 @@ for j in range(len(np.unique(segments_slic))):
     color[:, :, 1] = np.multiply(color[:, :, 1], indices)
     color[:, :, 2] = np.multiply(color[:, :, 2], indices)
 
-    regionR = color[color[:, :, 0] > 0, 0]
-    regionG = color[color[:, :, 1] > 0, 1]
-    regionB = color[color[:, :, 2] > 0, 2]
+    region_r = color[color[:, :, 0] > 0, 0]
+    region_g = color[color[:, :, 1] > 0, 1]
+    region_b = color[color[:, :, 2] > 0, 2]
 
     # plt.imshow(color, cmap='gray')
     # plt.title('SLIC'), plt.xticks([]), plt.yticks([])
     # plt.show()
 
-    MEAN = np.mean(regionR) + np.mean(regionG) + np.mean(regionB)
-    VARIANCE = np.var(regionR) + np.var(regionG) + np.var(regionB)
-    SKEWNESS = SCI.skew(regionR) + SCI.skew(regionG) + SCI.skew(regionB)
-    KURTOSIS = kurtosis(regionR) + kurtosis(regionG) + kurtosis(regionB)
-    ENTROPY = SCI.entropy(regionR) + SCI.entropy(regionG) + SCI.entropy(regionB)
-    imgFeatures = [MEAN, VARIANCE, SKEWNESS, KURTOSIS]
-    FEATURES = np.load('space_feature/features_' + str(conceito) + '.npy')
-    DIST_EUCLIDIAN = 0
-    target = np.ones([np.size(FEATURES, 0), 1])
-    PEARSON = []
-    for m in range(size(FEATURES, 0)):
-        # amostra = FEATURES[m, :]
-        amostra = FEATURES[m, 0:4]
-        # DIST_EUCLIDIAN += distance.euclidean(imgFeatures, amostra)
-        person = pearsonr(imgFeatures, amostra)
+    mean = np.mean(region_r) + np.mean(region_g) + np.mean(region_b)
+    variance = np.var(region_r) + np.var(region_g) + np.var(region_b)
+    skewness = SCI.skew(region_r) + SCI.skew(region_g) + SCI.skew(region_b)
+    kurtosis = kurtosis(region_r) + kurtosis(region_g) + kurtosis(region_b)
+    entropy = SCI.entropy(region_r) + SCI.entropy(region_g) + SCI.entropy(region_b)
+    img_features = [mean, variance, skewness, kurtosis]
+    features = np.load('space_feature/features_' + str(conceito) + '.npy')
+    dist_euclidian = 0
+    target = np.ones([np.size(features, 0), 1])
+    pearson = []
+    for m in range(size(features, 0)):
+        # amostra = features[m, :]
+        amostra = features[m, 0:4]
+        # dist_euclidian += distance.euclidean(img_features, amostra)
+        person = pearsonr(img_features, amostra)
         # print person
         if (person[1] < 0.05):
-            PEARSON.append(person[0])
+            pearson.append(person[0])
         # else:
-        #    PEARSON.append(0)
-    # print np.mean(PEARSON)
-    # print DIST_EUCLIDIAN
+        #    pearson.append(0)
+    # print np.mean(pearson)
+    # print dist_euclidian
 
     # gnb = GaussianNB()
-    # y_pred = gnb.fit(FEATURES, target).predict(imgFeatures)
+    # y_pred = gnb.fit(features, target).predict(img_features)
 
-    # if DIST_EUCLIDIAN >= 51000 or DIST_EUCLIDIAN <= 40000:
+    # if dist_euclidian >= 51000 or dist_euclidian <= 40000:
     # if y_pred == 1:
-    # if DIST_EUCLIDIAN <= 36000:
-    # print np.mean(np.abs(PEARSON))
-    if np.mean(np.abs(PEARSON)) > 0.99:
-        IMAGE_SEG[indices, 0] = 255
-        IMAGE_SEG[indices, 1] = 255
-        IMAGE_SEG[indices, 2] = 255
+    # if dist_euclidian <= 36000:
+    # print np.mean(np.abs(pearson))
+    if np.mean(np.abs(pearson)) > 0.99:
+        image_seg[indices, 0] = 255
+        image_seg[indices, 1] = 255
+        image_seg[indices, 2] = 255
     else:
-        IMAGE_SEG[indices, 0] = 0
-        IMAGE_SEG[indices, 1] = 0
-        IMAGE_SEG[indices, 2] = 0
+        image_seg[indices, 0] = 0
+        image_seg[indices, 1] = 0
+        image_seg[indices, 2] = 0
 
 
 a = np.array(img)
-a[:, :, 0] = np.multiply(img[:, :, 0], np.double(IMAGE_SEG[:, :, 0])/255.0)
-a[:, :, 1] = np.multiply(img[:, :, 1], np.double(IMAGE_SEG[:, :, 1])/255.0)
-a[:, :, 2] = np.multiply(img[:, :, 2], np.double(IMAGE_SEG[:, :, 2])/255.0)
+a[:, :, 0] = np.multiply(img[:, :, 0], np.double(image_seg[:, :, 0])/255.0)
+a[:, :, 1] = np.multiply(img[:, :, 1], np.double(image_seg[:, :, 1])/255.0)
+a[:, :, 2] = np.multiply(img[:, :, 2], np.double(image_seg[:, :, 2])/255.0)
 
 plt.subplot(131), plt.imshow(mark_boundaries(img, segments_slic))
 plt.title('Image'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(132), plt.imshow(IMAGE_SEG, cmap='binary')
+plt.subplot(132), plt.imshow(image_seg, cmap='binary')
 plt.title('SEGMENTACaO'), plt.xticks([]), plt.yticks([])
 
 plt.subplot(133), plt.imshow(a, cmap='gray')
@@ -102,9 +100,9 @@ plt.show()
 """
 EUCLIDIAN DISTANCE
 for i in range(10):
-    FEATURES = np.load('SpaceFeature/FEATURES_' + str(i) + '.npy')
-    DIST_EUCLIDIAN = 0
-    for j in range(size(FEATURES, 1)):
-        amostra = FEATURES[j, :]
-        dst = distance.euclidean(imgFeatures, amostra)
+    features = np.load('SpaceFeature/FEATURES_' + str(i) + '.npy')
+    dist_euclidian = 0
+    for j in range(size(features, 1)):
+        amostra = features[j, :]
+        dst = distance.euclidean(img_features, amostra)
 """
